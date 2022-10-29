ccb755a9a66486ac903d6980b49e188a4eb3eadd
slavon <vaceslavmartynov325@gmail.com>
2022-10-29 22:18:52 +0300

    fix: Rack App

--- app.rb
+++ app.rb
@@ -10,31 +10,29 @@ class App
 
   def perform_response(env)
     request = Rack::Request.new(env)
-
-    path  = request.path
     request_params = request.params['format']
+    return [400, headers, ["invalid_format_name\n"]] if request_params.nil?
 
-    if path != '/time'
-      return [404, headers, ["404\n"]]
-    elsif request_params.nil?
-      return [400, headers, ["invalid_format_name\n"]]
-    end
-
-    formatter = TimeFormatter.new(request_params)
-    if formatter.valid?
-      body = formatter.time
-      status = 200
-    else
-      invalid_params = formatter.invalid_params
-      body = "Unknown time format #{invalid_params}"
-      status = 400
-    end
-    [status, headers, ["#{body}\n"]]
+    call_formatter(request_params)
 
   end
 
+  def call_formatter(request_params)
+    formatter = TimeFormatter.new(request_params)
+    if formatter.valid?
+      body = formatter.call
+      status = 200
+    else
+      invalid_params = formatter.invalid.join(', ')
+      body = "Unknown time format #{invalid_params}"
+      status = 400
+
+    end
+    [status, headers, ["#{body}\n"]]
+  end
+
   def headers
-    { 'Content-Type' => 'text/plain'}
+    {}
   end
 
 end
--- time_formatter.rb
+++ time_formatter.rb
@@ -2,24 +2,32 @@ class TimeFormatter
 
   DESIGNATION = {'year'=> '%Y', 'month'=> '%m', 'day'=> '%d', 'hour'=> '%H', 'minute'=> '%m', 'second'=> '%S'}
 
-  attr_reader :params
+  attr_reader :invalid
 
   def initialize(params)
-    @params = params.split(',')
-  end
-
-  def time
-    body = self.params.reduce('') { |body_box, param| body_box << DESIGNATION[param] }
-    body = body.split('').join('-')
-    Time.now.strftime(body)
-  end
-
-  def invalid_params
-    self.params - DESIGNATION.keys
+    @valid = []
+    @invalid = []
+    @formats = params.split(',')
+    detect_formats
   end
 
   def valid?
-    invalid_params.empty?
+    invalid.empty?
   end
 
+  def call
+    Time.now.strftime(@valid.join('-'))
+  end
+
+  private
+
+  def detect_formats
+    @formats.each do |format|
+      if DESIGNATION[format]
+        @valid << DESIGNATION[format]
+      else
+        @invalid << format
+      end
+    end
+  end
 end
